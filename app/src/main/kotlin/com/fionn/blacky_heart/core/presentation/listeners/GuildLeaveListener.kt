bb5d5f0111c122fffbc4ed0186ebacc93b727a3e
Kazuki Mineshima <kazuki.mineshima@best-couple.love>
2025-10-21 15:28:37 +0900

    refactoring

--- app/src/main/kotlin/com/fionn/blacky_heart/core/presentation/listeners/GuildLeaveListener.kt
+++ app/src/main/kotlin/com/fionn/blacky_heart/core/presentation/listeners/GuildLeaveListener.kt
@@ -4,10 +4,6 @@ import com.fionn.blacky_heart.core.application.dtos.GuildDto
 import com.fionn.blacky_heart.core.application.services.GuildDeleteService
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-private val logger: Logger = LoggerFactory.getLogger(GuildLeaveListener::class.java)
 
 class GuildLeaveListener(
     private val guildDeleteService: GuildDeleteService
